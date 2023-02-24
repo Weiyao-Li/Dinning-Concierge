161c4745aeb7b18d6345f1de34bb1f392c824224
WeiyaoLi <wl2872@columbia.edu>
2023-02-24 16:31:13 -0500

    lf1, done

--- LF1/main.py
+++ LF1/main.py
@@ -14,7 +14,7 @@ logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
 # --- Helpers that build all of the responses ---
-sqs = boto3.client('sqs')
+
 
 
 def get_slots(intent_request):
@@ -45,7 +45,7 @@ def get_slot(intent_request, slotName):
     slots = get_slots(intent_request)
     if slots is not None and slotName in slots and slots[slotName] is not None:
         logger.debug('resolvedValue={}'.format(slots[slotName]['value']['resolvedValues']))
-        return slots[slotName]['value']['resolvedValues']
+        return slots[slotName]['value']['interpretedValue']
     else:
         return None
 
@@ -60,7 +60,7 @@ def elicit_slot(session_attributes, intent_request, slots, slot_to_elicit, slot_
                                         'state': 'InProgress'
                                         },
                              'sessionAttributes': session_attributes,
-                             'originatingRequestId': 'REQUESTID'
+                             'originatingRequestId': '70d49ca7-53de-4e1e-ac0a-70ecfc45b70a'
                              },
             'sessionId': intent_request['sessionId'],
             'messages': [message],
@@ -69,13 +69,13 @@ def elicit_slot(session_attributes, intent_request, slots, slot_to_elicit, slot_
             }
 
 
-def build_validation_result(is_valid, violated_slot, message_content):
-    return {
-        'isValid': is_valid,
-        'violatedSlot': violated_slot,
-        'message': {'contentType': 'PlainText', 'content': message_content}
-    }
-
+def build_validation_result(isvalid, violated_slot, slot_elicitation_style, message_content):
+    return {'isValid': isvalid,
+            'violatedSlot': violated_slot,
+            'slotElicitationStyle': slot_elicitation_style,
+            'message': {'contentType': 'PlainText',
+            'content': message_content}
+            }
 
 def GetItemInDatabase(postal_code):
     """
@@ -85,16 +85,22 @@ def GetItemInDatabase(postal_code):
     return None
 
 
-def close(session_attributes, fulfillment_state, message):
-    response = {
-        'sessionAttributes': session_attributes,
-        'dialogAction': {
-            'type': 'Close',
-            'fulfillmentState': fulfillment_state,
-            'message': message
-        }
+def close(intent_request, session_attributes, fulfillment_state, message):
+    intent_request['sessionState']['intent']['state'] = fulfillment_state
+    return {
+        'sessionState': {
+            'sessionAttributes': session_attributes,
+            'dialogAction': {
+                'type': 'Close'
+            },
+            'intent': intent_request['sessionState']['intent'],
+            'originatingRequestId': '2d3558dc-780b-422f-b9ec-7f6a1bd63f2e'
+        },
+        'messages': [ message ],
+        'sessionId': intent_request['sessionId'],
+        'requestAttributes': intent_request['requestAttributes'] if 'requestAttributes' in intent_request else None
     }
-    return response
+
 
 
 def parse_int(n):
@@ -103,120 +109,80 @@ def parse_int(n):
     except ValueError:
         return float('nan')
 
-
-def delegateSlot(intent_request):
+def delegate(intent_request, slots):
     return {
-        "dialogAction": {
-            "type": "Delegate",
-            "slots": intent_request["currentIntent"]["slots"]
-        }
-    }
+        "sessionState": {
+            "dialogAction": {
+                "type": "Delegate"
+            },
+            "intent": {
+                "name": intent_request['sessionState']['intent']['name'],
+                "slots": slots,
+                "state": "ReadyForFulfillment"
+            },
+            'sessionId': intent_request['sessionId'],
+            "requestAttributes": intent_request['requestAttributes'] if 'requestAttributes' in intent_request else None
+        }}
 
 
-def validate_phone_number(phone_number):
-    """
-    Validate the format of the phone number.
-    """
-    pattern = re.compile(r'^\d{10}$')  # pattern for 10-digit phone number
-    if not pattern.match(phone_number):
-        return build_validation_result(False, 'Phonenumber', 'Please enter a valid phone number with 10 digits.')
-    return build_validation_result(True, None, None)
-
-
-def delegate(session_attributes, slots):
-    return {
-        'sessionAttributes': session_attributes,
-        'dialogAction': {
-            'type': 'Delegate',
-            'slots': slots
-        }
-    }
-
-
-def validationProcess(Location, Cuisine, Date, Time, Numberofpeople, Phonenumber):
+def validationProcess(Location, Cuisine, Date, Time, Numberofpeople, Email):
     # Location Validation
-    if Location and Location.lower() not in ['new york city', 'manhattan', 'bronx', 'queens', 'nyc', 'new york']:
-        return build_validation_result(False,
-                                       'location',
+    if Location:
+        print("12312313131")
+        if Location.lower() not in ['new york city', 'manhattan', 'bronx', 'queens', 'nyc', 'new york']:
+            return build_validation_result(False,
+                                       'Location',
+                                       'SpellbyWord',
                                        'Currently this is not an available location. Please enter location, like Manhattan')
-
+        else:
+            print("4546456456464")
+            return True
     # Cuisine Validation:
-    if Cuisine and Cuisine.lower() not in ['chinese', 'japanese', 'thai', 'american', 'french', 'italian', 'indian']:
-        return build_validation_result(False, 'Cuisine', 'Sorry! The one you just entered is invalid! '
+    if Cuisine:
+        if Cuisine.lower() not in ['chinese', 'japanese', 'thai', 'american', 'french', 'italian', 'indian']:
+            return build_validation_result(False, 'Cuisine', 'SpellbyWord', 'Sorry! The one you just entered is invalid! '
                                                          'Please choose from the following options: '
                                                          'chinese, japanese, thai, american, french, italian, indian')
-
+        else:
+            return True
     # Numberofpeople Validation
 
     if Numberofpeople:
         if int(Numberofpeople) not in range(1, 11):
             return build_validation_result(False,
                                            'Numberofpeople',
-                                           'Number of people should be between 1 and 10')
-    # Phonenumber Validation
-    if Phonenumber:
-        if not validate_phone_number(Phonenumber):
-            return build_validation_result(False,
-                                           'Phonenumber',
-                                           'The phone number you entered is not valid. Please enter a valid phone number.')
-    else:
-        return build_validation_result(False,
-                                       'Phonenumber',
-                                       'Please provide a phone number.')
+                                           'SpellbyWord',
+                                            'Number of people should be between 1 and 10')
+        else:
+            return True
 
     # Date Validation
-    if not Date:
-        # 'Date' slot is empty, delegate to Lex to prompt the user for input.
-        return delegateSlot()
-
-    try:
-        entered = datetime.strptime(Date, '%Y-%m-%d')
-        cur_time = datetime.now()
-
-        if entered.date() < cur_time.date():
-            # Reservation date is in the past, elicit 'Date' slot and prompt the user for a valid date.
-            return elicitSlotExecute('Date', build_validation_result(
-                False, 'Date', "Oh. We can't make a reservation in the past. Could you please enter a valid date?"))
-
-        if entered.date() == cur_time.date():
-            if not Time:
-                # 'Time' slot is empty, elicit 'Time' slot and prompt the user for a valid time.
-                return elicitSlotExecute('Time', build_validation_result(
-                    False, 'Time', "Please provide a valid time in the format HH:MM."))
-
-            try:
-                entered_time = datetime.strptime(Time, '%H:%M').time()
-                if entered_time < cur_time.time():
-                    # Reservation time is in the past, elicit 'Time' slot and prompt the user for a valid time.
-                    return elicitSlotExecute('Time', build_validation_result(
-                        False, 'Time',
-                        "Oh. We can't make a reservation for a time in the past. Could you please enter a valid time?"))
-            except ValueError:
-                # User entered an invalid time format, elicit 'Time' slot and prompt the user for a valid time.
-                return elicitSlotExecute('Time', build_validation_result(
-                    False, 'Time',
-                    "Sorry, I didn't understand that. Could you please enter a valid time in the format HH:MM?"))
-
-        # Reservation date and time are valid, delegate to Lex to continue with the next step of the conversation.
-        return delegateSlot()
-
-    except ValueError:
-        # User entered an invalid date format, elicit 'Date' slot and prompt the user for a valid date.
-        return elicitSlotExecute('Date', build_validation_result(
-            False, 'Date',
-            "Sorry, I didn't understand that. Could you please enter a valid date in the format yyyy-mm-dd?"))
-
+    if Date:
+        print("Debug: date is:", Date)
+        year, month, day = map(int, Date.split('-'))
+        date_to_check = datetime.date(year, month, day)
+        if date_to_check < datetime.date.today():
+            return build_validation_result(False, 'date', 'SpellByWord', 'Please enter a valid Dining date')
+        else:
+            return True
+    if Time:
+        print("Debug: time is:", Time)
+        if not Time:
+            return build_validation_result(False, 'Time', 'SpellByWord', '')
+        if Time:
+            return True
 
 def DiningSuggestionsIntent(intent_request):
     state = intent_request['sessionState']
-    slots = intent_request["intent"]["slots"]
 
-    Location = get_slots(intent_request)["Location"]
-    Cuisine = get_slots(intent_request)["Cuisine"]
-    Date = get_slots(intent_request)["Date"]
-    Time = get_slots(intent_request)["Time"]
-    Numberofpeople = get_slots(intent_request)["Numberofpeople"]
-    Phonenumber = get_slots(intent_request)["Phonenumber"]
+    Location = get_slot(intent_request, "Location")
+    Cuisine = get_slot(intent_request, "Cuisine")
+    Date = get_slot(intent_request, "Date")
+    Time = get_slot(intent_request, "Time")
+    Numberofpeople = get_slot(intent_request, "Numberofpeople")
+    Email = get_slot(intent_request, "Email")
+
+    session_attributes = get_session_attributes(intent_request)
 
     # type of event that triggered the function
     source = intent_request['invocationSource']
@@ -226,21 +192,31 @@ def DiningSuggestionsIntent(intent_request):
 
         slots = get_slots(intent_request)
 
-        resOfValidation = validationProcess(Location, Cuisine, Date, Time, Numberofpeople, Phonenumber)
-        if not resOfValidation['isValid']:
+        resOfValidation = validationProcess(Location, Cuisine, Date, Time, Numberofpeople, Email)
+        if not resOfValidation:
             slots[resOfValidation['violatedSlot']] = None
-            return elicit_slot(intent_request['sessionAttributes'], intent_request['intent']['name'], slots,
-                               resOfValidation['violatedSlot'], resOfValidation['message'])
+            return elicit_slot(session_attributes, intent_request, slots,
+                               resOfValidation['violatedSlot'], resOfValidation['slotElicitationStyle'], resOfValidation['message'])
 
-        output_session_attributes = intent_request['sessionAttributes'] if intent_request[
-                                                                               'sessionAttributes'] is not None else {}
+    if not Location or not Date or not Time or not Numberofpeople or not Email or not Cuisine:
+        return delegate(intent_request, get_slots(intent_request))
+    else:
+        send_message_to_SQS(
+            Location,
+            Cuisine,
+            Date,
+            Time,
+            Numberofpeople,
+            Email
 
-        return delegate(output_session_attributes, get_slots(intent_request))
 
-    return close(intent_request['sessionAttributes'],
-                 'Fulfilled',
-                 {'contentType': 'PlainText',
-                  'content': 'Thanks. We will send you email shortly'})
+        )
+
+        return close(intent_request,
+                     session_attributes,
+                     'Fulfilled',
+                     {'contentType': 'PlainText',
+                      'content': 'Thanks. We will send you email shortly'})
 
 
 # --- Intents ---
@@ -250,28 +226,47 @@ def dispatch(intent_request):
     Called when the user specifies an intent for this bot.
     """
     intent_name = intent_request['sessionState']['intent']['name']
-    state = intent_request['sessionState']
+    # state = intent_request['sessionState']
 
     if intent_name == 'DiningSuggestionsIntent':
         return DiningSuggestionsIntent(intent_request)
     print("Error!", intent_name)
 
 
-def send_message_to_SQS(slots):
-    msg = {
-        'Location': slots['Location'],
-        'Cuisine': slots['Cuisine'],
-        'Date': slots['Date'],
-        'Time': slots['Time'],
-        'Numberofpeople': slots['Numberofpeople'],
-        'Phonenumber': slots['Phonenumber']
-    }
+
+def send_message_to_SQS(Location, Cuisine, Date, Time, Numberofpeople, Email):
+    sqs = boto3.client('sqs')
 
     response = sqs.send_message(
         QueueUrl="https://sqs.us-east-1.amazonaws.com/778348423801/RestaurantQueue",
-        DelaySeconds=0,
-        MessageBody=json.dumps(msg),
-        MessageGroupId='LF1'
+        MessageAttributes={
+            'Location': {
+                'DataType': 'String',
+                'StringValue': Location
+            },
+            'Cuisine': {
+                'DataType': 'String',
+                'StringValue': Cuisine
+            },
+            'Date': {
+                'DataType': 'String',
+                'StringValue': Date
+            },
+            'Time': {
+                'DataType': 'String',
+                'StringValue': Time
+            },
+            'Numberofpeople': {
+                'DataType': 'Number',
+                'StringValue': str(Numberofpeople)
+            },
+            'Email': {
+                'DataType': 'String',
+                'StringValue': Email
+            }
+        },
+
+        MessageBody=('Information about user inputs of Dining Chatbot.'),
     )
 
 # --- Main handler ---
