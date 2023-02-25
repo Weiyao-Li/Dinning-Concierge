9df1b4098f54dee7273fe836f077b619b3b36245
Vedangi Wagh <40696836+vedangiwagh@users.noreply.github.com>
2023-02-24 20:30:08 -0500

    combiningjsonfiles

---
+++ combinejsondata.py
@@ -0,0 +1,19 @@
+import os
+import json
+
+
+folder_path = "C:/Users/Dell/OneDrive/Documents/ccbd/CCBD-Assignment1-main/restaurants"
+
+
+combined_data = {}
+
+files=[ 'italian_data.json', 'ethiopian_data.json','chinese_data.json', 'american_data.json', 'mexican_data.json', 'japanese_data.json', 'indian_data.json','french_data.json','spanish_data.json']
+
+result = list()
+for f1 in files:
+    with open(f1, 'r') as infile:
+        result.extend(json.load(infile))
+
+with open('data.json', 'w') as output_file:
+    json.dump(result, output_file)
+
