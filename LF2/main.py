759f42fa1b3692fd8ca4e56dfb378256629e0d8e
WeiyaoLi <wl2872@columbia.edu>
2023-02-25 16:12:02 -0500

    LF2 initial commit

---
+++ LF2/main.py
@@ -0,0 +1,66 @@
+import json
+import os
+
+import boto3
+from opensearchpy import OpenSearch, RequestsHttpConnection
+from requests_aws4auth import AWS4Auth
+
+REGION = 'us-east'
+HOST = 'search-restaurants-td6nk4e3zgzg3i3ix5ezgpr7am.us-east-1.es.amazonaws.com/'
+INDEX = 'restaurants'
+
+
+
+
+def query(term):
+    q = {'size': 5, 'query': {'multi_match': {'query': term}}}
+
+    client = OpenSearch(hosts=[{
+        'host': HOST,
+        'port': 443
+    }],
+                        http_auth=get_awsauth(REGION, 'es'),
+                        use_ssl=True,
+                        verify_certs=True,
+                        connection_class=RequestsHttpConnection)
+
+    res = client.search(index=INDEX, body=q)
+    print(res)
+
+    hits = res['hits']['hits']
+    results = []
+    for hit in hits:
+        results.append(hit['_source'])
+
+    return results
+
+
+
+
+
+
+
+def lambda_handler(event, context):
+    print('Received event: ' + json.dumps(event))
+
+    results = query('Aaron Rodgers')
+
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': '*',
+        },
+        'body': json.dumps({'results': results})
+    }
+
+
+# def get_awsauth(region, service):
+#     cred = boto3.Session().get_credentials()
+#     return AWS4Auth(cred.access_key,
+#                     cred.secret_key,
+#                     region,
+#                     service,
+#                     session_token=cred.token)
